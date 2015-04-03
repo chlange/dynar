cf304d6229d41996308300df4d888cc91c8e5591
Christian Lange <Christian_Lange@hotmail.com>
2015-04-03 10:35:18 +0200

    Fixed return values for a conclusive API for the index search functions.

--- dynar.h
+++ dynar.h
@@ -1,13 +1,13 @@
 #ifndef __DYNAR_H__
 #define __DYNAR_H__
 
-#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
-/** 
+/**
  * @file
  * @brief Header file for the dynamic array.
- * 
+ *
  * The section @b Errors of each function description refers to the returned @p err value of the function.
  */
 
@@ -19,7 +19,7 @@
 
 /**
  * Group of parameter errors.
- * 
+ *
  * The actual parameter error gets bitwise OR'ed with this value.
  */
 #define DA_PARAM_ERR     0x10000000
@@ -39,7 +39,7 @@
 
 /**
  * Group of fatal errors.
- * 
+ *
  * The actual parameter error gets bitwise OR'ed with this value.
  */
 #define DA_FATAL         0x20000000
@@ -78,17 +78,19 @@
 /**
  * @brief The structure defines the initial setup for an array.
  */
-typedef struct __str_da_desc 
+typedef struct __str_da_desc
 {
-    /** 
+    /**
      * Initial amount of elements for the array.
+     * Must be greater than 0.
      */
-	size_t elements;
-    
+    size_t elements;
+
     /**
      * Amount of bytes each element consists of.
+     * Must be greater than 0.
      */
-	size_t bytesPerElement;
+    size_t bytesPerElement;
 
 } DaDesc;
 
@@ -100,9 +102,9 @@ typedef struct __str_da_ptr
     /**
      * First address of the array.
      */
-	void *firstAddr;
+    void *firstAddr;
 
-	/**
+    /**
      * Last address of the array.
      */
     void *lastAddr;
@@ -110,26 +112,26 @@ typedef struct __str_da_ptr
     /**
      * Next free address in the array.
      */
-	void *freeAddr;
+    void *freeAddr;
 
     /**
      * Amount of elements currently used.
      */
-	size_t used;
+    size_t used;
 
     /**
      * Amount of maximum elements in the current array.
      */
-	size_t max;
+    size_t max;
 
-	/**
+    /**
      * Amount of bytes each element consists of.
      */
     size_t bytesPerElement;
 
     /**
      * Magic number to avoid use-after-free or similar errors.
-     * 
+     *
      * Gets set to ::DA_MAGIC when the array gets created and set to 0 when it gets destroyed.
      */
     int magic;
@@ -144,14 +146,14 @@ typedef struct __str_da_ptr
  *
  * @param[in]  desc Initial settings for the array.
  * @param[out] err  Indicates what went wrong in the event of an error.
- * 
+ *
  * @returns Returns a pointer to the dynamic array on success that can be successfully passed to DaDestroy().
  * @returns Otherwise, a NULL pointer is returned and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_FATAL | ::DA_ENOMEM if no space is left on device.@n
- * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
+ * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p desc is a NULL-pointer. @n
  */
 DaStruct *daCreate(DaDesc *desc, int *err);
 
@@ -160,15 +162,13 @@ DaStruct *daCreate(DaDesc *desc, int *err);
  *
  * The function frees all allocated memory of the dynamic array @p da.
  * @p da gets invalid after successful execution.
- * 
- * The array remains unchanged in the event of an error.
- * 
- * @param[in]  da The array that should be destroyed.
+ *
+ * @param[in]  da  The array that should be destroyed.
  * @param[out] err Indicates what went wrong in the event of an error.
  *
  * @returns Returns 0 on success.
  * @returns Otherwise, -1 is returned and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -178,12 +178,12 @@ int daDestroy(DaStruct *da, int *err);
 /**
  * @brief The function returns the number of elements in the array.
  *
- * @param[in]  da  Return the number of elements of this array. 
+ * @param[in]  da  Return the number of elements of this array.
  * @param[out] err Indicates what went wrong in the event of an error.
- * 
+ *
  * @returns The function returns the number of elements of the array on success.
- * @returns Keep in mind to check the value of @p err.
- * 
+ * @returns Otherwise, the function returns 0 and @p err is set appropriately.
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -195,9 +195,9 @@ size_t daSize(DaStruct *da, int *err);
  *
  * @param[in]  da  Return the fill state of this array.
  * @param[out] err Indicates what went wrong in the event of an error.
- * 
+ *
  * @returns The function returns 1 if the array is empty or 0 if it's not empty.
- * @returns Keep in mind to check the value of @p err.
+ * @returns Otherwise, the function returns -10 and @p err is set appropriately.
  *
  * @b Errors @n
  * ::DA_OK on success. @n
@@ -209,21 +209,21 @@ int daIsEmpty(DaStruct *da, int *err);
  * @brief The function appends the @p element to the array.
  *
  * Shifts all elements in the array one position to the right before appending the new @p element.
- * 
+ *
  * The array will be increased if necessary.
  * It remains unchanged in the event of an error.
- * 
+ *
  * @param[in]  da      Append the element to this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  element The element that shall be appended.
  *
  * @returns The function returns a pointer to the appended element on success.
  * @returns Otherwise, the function returns a NULL pointer and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_FATAL | ::DA_ENOMEM if no space is left on device.@n
- * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. 
+ * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer.
  */
 void *daAppend(DaStruct *da, int *err, void *element);
 
@@ -243,7 +243,7 @@ void *daAppend(DaStruct *da, int *err, void *element);
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_FATAL | ::DA_ENOMEM if no space is left on device.@n
- * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. 
+ * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer.
  */
 void *daPrepend(DaStruct *da, int *err, void *element);
 
@@ -251,10 +251,10 @@ void *daPrepend(DaStruct *da, int *err, void *element);
  * @brief The function inserts the @p element at @p pos.
  *
  * Shifts the element and any subsequent elements from @p pos one position to the right before inserting the new @p element.
- * 
+ *
  * @p pos must be in between the array bounds.
  * The array remains unchanged in the event of an error.
- * 
+ *
  * @param[in]  da      Insert the element into this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  element The element that shall be inserted.
@@ -262,7 +262,7 @@ void *daPrepend(DaStruct *da, int *err, void *element);
  *
  * @returns The function returns a pointer to the inserted element on success.
  * @returns Otherwise, the function returns a NULL pointer and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_FATAL | ::DA_ENOMEM if no space is left on device. @n
@@ -275,16 +275,16 @@ void *daInsertAt(DaStruct *da, int *err, void *element, size_t pos);
  * @brief The function deletes the element at @p pos.
  *
  * Shifts all subsequent elements from @p pos one position to the left.
- * 
+ *
  * @p pos must be in between the array bounds.
  * The array remains unchanged in the event of an error.
- * 
+ *
  * @param[in]  da      Delete the element from this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  pos     Delete the @p element at this position from the array.
- * 
+ *
  * @returns Returns 0 on success, otherwise -1 is returned and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -294,14 +294,14 @@ int daRemove(DaStruct *da, int *err, size_t pos);
 
 /**
  * @brief The function returns the element at @p pos.
- * 
+ *
  * @param[in]  da      Get the element from this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  pos     Get the @p element at this position from the array.
- * 
+ *
  * @returns The function returns a pointer to the element on success.
  * @returns Otherwise, the function returns a NULL pointer and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -311,13 +311,13 @@ void *daGet(DaStruct *da, int *err, size_t pos);
 
 /**
  * @brief The function returns the first element of the array
- * 
+ *
  * @param[in]  da      Get the element from this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
- * 
+ *
  * @returns The function returns a pointer to the first element on success.
  * @returns Otherwise, the function returns a NULL pointer and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -327,13 +327,13 @@ void *daGetFirst(DaStruct *da, int *err);
 
 /**
  * @brief The function returns the last element of the array
- * 
+ *
  * @param[in]  da      Get the element from this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
- * 
+ *
  * @returns The function returns a pointer to the last element on success.
  * @returns Otherwise, the function returns a NULL pointer and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -345,7 +345,7 @@ void *daGetLast(DaStruct *da, int *err);
  * @brief The function replaces the element at @p pos with the contents from the specified @p element.
  *
  * The function copies the bytes from the specified @p element to the element at @p pos.
- * 
+ *
  * The array remains unchanged in the event of an error.
  *
  * @param[in]  da         Update the element of this array.
@@ -355,7 +355,7 @@ void *daGetLast(DaStruct *da, int *err);
  *
  * @returns The function returns a pointer to the updated element on success.
  * @returns Otherwise, the function returns a NULL pointer and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -365,7 +365,7 @@ void *daSet(DaStruct *da, int *err, void *element, size_t pos);
 
 /**
  * @brief The function increases the array in a way that @p n free slots for new elements are available.
- * 
+ *
  * The array remains unchanged in the event of an error.
  *
  * @param[in]  da   Increase this array.
@@ -376,9 +376,9 @@ void *daSet(DaStruct *da, int *err, void *element, size_t pos);
  * @b Modes @n
  *   @p ::DA_HARD: Increase space for exactly @p n new elements no matter if enough space is available.@n
  *   @p ::DA_SOFT: Increase the array only if not enough space for @p n new elements is available.
- *   
+ *
  * @returns Returns 0 on success, otherwise, -1 is returned and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_FATAL | ::DA_ENOMEM if no space is left on device. @n
@@ -389,13 +389,13 @@ int daIncrease(DaStruct *da, int *err, size_t n, int mode);
 
 /**
  * @brief Removes all elements from the array.
- * 
+ *
  * @param[in]  da  Clear this array.
  * @param[out] err Indicates what went wrong in the event of an error.
  *
  * @returns Returns 0 on success, otherwise, -1 is returned and @p err is set appropriately.
- * @returns In the case of an empty array 0 is returned.
- * 
+ * @returns In the case of an already empty array 0 is returned.
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer. @n
@@ -404,13 +404,14 @@ int daClear(DaStruct *da, int *err);
 
 /**
  * @brief Checks whether the array contains the @p element.
- * 
+ *
  * @param[in]  da      Search this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  element Search the array for this element.
- * 
- * @returns Returns 1 if the array contains the @p element, otherwise 0 is returned.
- * 
+ *
+ * @returns Returns 1 if the array contains the @p element and 0 if not.
+ * @returns The function returns -1 in the event of an error and @p err is set appropriately.
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_NOT_FOUND if the array doesn't contain an element equal to the specified @p element (e.g. the array is empty). @n
@@ -420,46 +421,50 @@ int daContains(DaStruct *da, int *err, void *element);
 
 /**
  * @brief Returns the index of the first occurence of the @p element in the array.
- * 
+ *
  * @param[in]  da      Search this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  element Search the array for this element.
+ * @param[out] index   Index of the @p element if the array contains it
  *
- * @returns Returns the index of the first occurence on success.
- * @returns Keep in mind to check the value of @p err.
- * 
+ * @returns Returns  1 if the array contains the @p element and the index is returned via @p index.
+ * @returns Returns  0 if the array doesn't contain the @p element.
+ * @returns Returns -1 in the event of an error and @p err is set appropriately.
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_NOT_FOUND if the array doesn't contain an element equal to the specified @p element (e.g. the array is empty). @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer.
  */
-size_t daIndexOf(DaStruct *da, int *err, void *element);
+int daIndexOf(DaStruct *da, int *err, void *element, size_t *index);
 
 /**
  * @brief Returns the index of the last occurence of the @p element in the array.
- * 
+ *
  * @param[in]  da      Search this array.
  * @param[out] err     Indicates what went wrong in the event of an error.
  * @param[in]  element Search the array for this element.
+ * @param[out] index   Index of the @p element if the array contains it.
  *
- * @returns Returns the index of the last occurence on success.
- * @returns Keep in mind to check the value of @p err.
- * 
+ * @returns Returns 1  if the array contains the @p element and the index is returned via @p index.
+ * @returns Returns 0  if the array doesn't contain the @p element.
+ * @returns Returns -1 in the event of an error and @p err is set appropriately.
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_NOT_FOUND if the array doesn't contain an element equal to the specified @p element (e.g. the array is empty). @n
  * ::DA_PARAM_ERR | ::DA_PARAM_NULL if @p da is a NULL-pointer.
  */
-size_t daLastIndexOf(DaStruct *da, int *err, void *element);
+int daLastIndexOf(DaStruct *da, int *err, void *element, size_t *index);
 
 /**
  * @brief Returns a copy of the array.
- * 
+ *
  * @param[in]  da  Clone this array.
  * @param[out] err Indicates what went wrong in the event of an error.
  *
  * @returns Returns a pointer to the cloned array on success, otherwise a NULL pointer is returned and @p err is set appropriately.
- * 
+ *
  * @b Errors @n
  * ::DA_OK on success. @n
  * ::DA_FATAL | ::DA_ENOMEM if no space is left on device. @n
