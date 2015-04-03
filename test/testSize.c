c7455273c2454457fdec2decc04076c1dfa41a7e
Christian Lange <Christian_Lange@hotmail.com>
2015-04-03 22:24:08 +0200

    Refined test descriptions. Use MAGIC. Added MAGIC test.
    * Refined test descriptions.
    
    * Set MAGIC number in array header.
    
    * Added MAGIC test.

--- test/testSize.c
+++ test/testSize.c
@@ -4,8 +4,8 @@
 static void testNull(void)
 {
     int err;
-    DaStruct da;
     size_t n;
+    DaStruct da;
 
     sput_fail_if(daSize(NULL, NULL, &n)   != -1, "daSize(NULL, NULL, &n)   != -1");
     sput_fail_if(daSize(NULL, NULL, NULL) != -1, "daSize(NULL, NULL, NULL) != -1");
@@ -21,24 +21,37 @@ static void testNull(void)
 static void testValid(void)
 {
     int err;
-    DaStruct da;
     size_t n;
+    DaStruct da;
+
+    da.magic = DA_MAGIC;
 
     da.used = 0;
-    sput_fail_if(daSize(&da, &err, &n) != 0, "daSize(da, &err, n) != 0");
+    sput_fail_if(daSize(&da, &err, &n) != 0, "daSize(da, &err, 0) should return 0");
     sput_fail_if(n != 0, "daSize returned an incorrect size.");
     sput_fail_if(err != DA_OK, "err != DA_OK");
 
     da.used = 10;
-    sput_fail_if(daSize(&da, &err, &n) != 0, "daSize(da, &err, n) != 0");
+    sput_fail_if(daSize(&da, &err, &n) != 0, "daSize(da, &err, 10) should return 0");
     sput_fail_if(n != 10, "daSize returned an incorrect size.");
     sput_fail_if(err != DA_OK, "err != DA_OK");
 
     da.used = 50;
-    sput_fail_if(daSize(&da, &err, &n) != 0, "daSize(da, &err, n) != 0");
+    sput_fail_if(daSize(&da, &err, &n) != 0, "daSize(da, &err, 50) should return 0");
     sput_fail_if(n != 50, "daSize returned an incorrect size.");
     sput_fail_if(err != DA_OK, "err != DA_OK");
+}
 
+static void testMagic(void)
+{
+    int err;
+    size_t n;
+    DaStruct da;
+
+    da.magic = DA_MAGIC + 1;
+
+    sput_fail_if(daSize(&da, &err, &n) != -1, "daSize(...) should fail if the magic number mismatches");
+    sput_fail_if(err != (DA_PARAM_ERR | DA_PARAM_NULL), "err != (DA_PARAM_ERR | DA_PARAM_NULL)");
 }
 
 int main(void)
@@ -50,6 +63,9 @@ int main(void)
 
     sput_enter_suite("daSize should return the size of the array");
     sput_run_test(testValid);
+
+    sput_enter_suite("daSize should fail if the magic number in the header mismatches the expected magic number");
+    sput_run_test(testMagic);
 
     sput_finish_testing();
 
