cf12677c02ec01fd8d24355ad6292e00f57e0b22
Christian Lange <Christian_Lange@hotmail.com>
2015-04-03 22:26:39 +0200

    Refined test descriptions. Use MAGIC. Added MAGIC test.
    * Refined test descriptions.
    
    * Set MAGIC number in array header.
    
    * Added MAGIC test.

--- test/testIsEmpty.c
+++ test/testIsEmpty.c
@@ -18,17 +18,29 @@ static void testValid(void)
     int err;
     DaStruct da;
 
+    da.magic = DA_MAGIC;
     da.used = 0;
-    sput_fail_if(daIsEmpty(&da, &err) != 1, "daIsEmpty(da, &err) != 1");
+    sput_fail_if(daIsEmpty(&da, &err) != 1, "daIsEmpty(...) should return 1 if array is empty");
     sput_fail_if(err != DA_OK, "err != DA_OK");
 
     da.used = 1;
-    sput_fail_if(daIsEmpty(&da, &err) != 0, "daIsEmpty(da, &err) != 0");
+    sput_fail_if(daIsEmpty(&da, &err) != 0, "daIsEmpty(...) should return 0 if array isn't empty");
     sput_fail_if(err != DA_OK, "err != DA_OK");
 
     da.used = 2;
-    sput_fail_if(daIsEmpty(&da, &err) != 0, "daIsEmpty(da, &err) != 0");
+    sput_fail_if(daIsEmpty(&da, &err) != 0, "daIsEmpty(...) should return 0 if array isn't empty");
     sput_fail_if(err != DA_OK, "err != DA_OK");
+}
+
+static void testMagic(void)
+{
+    int err;
+    DaStruct da;
+
+    da.magic = DA_MAGIC + 1;
+
+    sput_fail_if(daIsEmpty(&da, &err) != -1, "daIsEmpty(...) should fail if the magic number mismatches");
+    sput_fail_if(err != (DA_PARAM_ERR | DA_PARAM_NULL), "err != (DA_PARAM_ERR | DA_PARAM_NULL)");
 }
 
 int main(void)
@@ -40,6 +52,9 @@ int main(void)
 
     sput_enter_suite("daIsEmpty should return the fill state of the array");
     sput_run_test(testValid);
+
+    sput_enter_suite("daIsEmpty should fail if the magic number in the header mismatches the expected magic number");
+    sput_run_test(testMagic);
 
     sput_finish_testing();
 
